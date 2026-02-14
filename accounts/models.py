c52136cca338e5505ced68fc82aff5196ea1b6d2
Sharath Kumar <Shkumar@synamedia.com>
2026-02-14 13:18:15 +0530

    Create UserModel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', User.Role.ADMIN)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('role') != User.Role.ADMIN:
+            raise ValueError('Superuser must have role=ADMIN.')
+
+        return self.create_user(email, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin',
+        CITIZEN = 'CITIZEN', 'Citizen',
+        COLLECTOR = 'COLLECTOR', 'Collector'
+
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=30)
+
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.CITIZEN)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name']
+
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"
+
 
 # Create your models here.
--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-in'
 
 TIME_ZONE = 'UTC'
 
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'accounts.User'
